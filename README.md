9d10d439895adc6374c84a4f361babd7406f459d
Emanuel Facundo <38675971+EmanuelFacundo@users.noreply.github.com>
2021-03-24 15:50:22 -0300

    Trabalhando com Sockets HTTP e Arquitetura Orientada a Eventos

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # SemanaJSExpert
  Semana JS Expert com Erick Wendel de 22/03 a 26/03
 
- Criando interfaces em programas de linha de comando com o pacote Blessed e o padrão Builder
+ #1 Criando interfaces em programas de linha de comando com o pacote Blessed e o padrão Builder
--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -1,8 +1,24 @@
+/*
+node index.js \
+    --username emaneulfp
+    --room sala01 \
+    --hostUri localhost
+*/
+
+
 import Events from "events";
-
 import TerminalController from "./src/terminalContrller.js";
+import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
 
+const [nodePath, filePath, ...commands] = process.argv
+const config = CliConfig.parseArguments(commands)
+console.log('config', config)
 const componentEmitter = new Events()
 
-const controller = new TerminalController()
-await controller.initializeTable(componentEmitter)
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
+// const componentEmitter = new Events()
+
+// const controller = new TerminalController()
+// await controller.initializeTable(componentEmitter)
